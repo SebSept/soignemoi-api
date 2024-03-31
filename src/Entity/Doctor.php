e64efb959882b86d8a57ed2ffd1c7cb12d1b817a
SebSept <sebastienmonterisi@yahoo.fr>
2024-03-31 16:54:58 +0200

    Nettoyage entité Doctor

--- src/Entity/Doctor.php
+++ src/Entity/Doctor.php
@@ -7,26 +7,14 @@ use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Post;
-use App\Controller\RecentDoctors;
 use App\Repository\DoctorRepository;
-use ContainerUWOM2sD\getMedicalOpinionRepositoryService;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Attribute\Groups;
-use Symfony\Component\Serializer\Attribute\Ignore;
 
 #[ORM\Entity(repositoryClass: DoctorRepository::class)]
-//#[GetCollection(uriTemplate: '/doctors/recent', controller: RecentDoctors::class, normalizationContext: ['groups' => ['read']], name: 'recent',)]
 #[ApiResource(
     operations: [
         new GetCollection(),
-        // non défini ici, ne fonctionne pas, finalement oui... mais uniquement si on a pas de Get() avant.
-        new GetCollection(
-            uriTemplate: '/doctors/recent',
-            controller: RecentDoctors::class,
-            name: 'recent', // method
-        ),
         new Get(),
         new Post(),
         new Patch(),
@@ -34,7 +22,6 @@ use Symfony\Component\Serializer\Attribute\Ignore;
     normalizationContext: ['groups' => ['read']],
     denormalizationContext: ['groups' => ['write']]
 )
-
 ]
 class Doctor
 {
@@ -62,25 +49,6 @@ class Doctor
     #[ORM\Column(length: 255)]
     #[Groups(['write'])]
     private ?string $password = null;
-
-    // @todo supprimer tout simplement ?
-    #[ORM\OneToMany(targetEntity: MedicalOpinion::class, mappedBy: 'doctor')]
-    private Collection $medicalOpinions;
-
-    // @todo supprimer tout simplement ?
-    #[ORM\OneToMany(targetEntity: HospitalStay::class, mappedBy: 'doctor')]
-    private Collection $hospitalStays;
-
-    // @todo supprimer tout simplement ?
-    #[ORM\OneToMany(targetEntity: Prescription::class, mappedBy: 'doctor')]
-    private Collection $prescriptions;
-
-    public function __construct()
-    {
-        $this->medicalOpinions = new ArrayCollection();
-        $this->hospitalStays = new ArrayCollection();
-        $this->prescriptions = new ArrayCollection();
-    }
 
     public function getId(): ?int
     {
@@ -143,96 +111,6 @@ class Doctor
     public function setPassword(string $password): static
     {
         $this->password = $password;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, MedicalOpinion>
-     */
-    public function getMedicalOpinions(): Collection
-    {
-        return $this->medicalOpinions;
-    }
-
-    public function addMedicalOpinion(MedicalOpinion $medicalOpinion): static
-    {
-        if (!$this->medicalOpinions->contains($medicalOpinion)) {
-            $this->medicalOpinions->add($medicalOpinion);
-            $medicalOpinion->setDoctor($this);
-        }
-
-        return $this;
-    }
-
-    public function removeMedicalOpinion(MedicalOpinion $medicalOpinion): static
-    {
-        if ($this->medicalOpinions->removeElement($medicalOpinion)) {
-            // set the owning side to null (unless already changed)
-            if ($medicalOpinion->getDoctor() === $this) {
-                $medicalOpinion->setDoctor(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, HospitalStay>
-     */
-    public function getHospitalStays(): Collection
-    {
-        return $this->hospitalStays;
-    }
-
-    public function addHospitalStay(HospitalStay $hospitalStay): static
-    {
-        if (!$this->hospitalStays->contains($hospitalStay)) {
-            $this->hospitalStays->add($hospitalStay);
-            $hospitalStay->setDoctor($this);
-        }
-
-        return $this;
-    }
-
-    public function removeHospitalStay(HospitalStay $hospitalStay): static
-    {
-        if ($this->hospitalStays->removeElement($hospitalStay)) {
-            // set the owning side to null (unless already changed)
-            if ($hospitalStay->getDoctor() === $this) {
-                $hospitalStay->setDoctor(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Prescription>
-     */
-    public function getPrescriptions(): Collection
-    {
-        return $this->prescriptions;
-    }
-
-    public function addPrescription(Prescription $prescription): static
-    {
-        if (!$this->prescriptions->contains($prescription)) {
-            $this->prescriptions->add($prescription);
-            $prescription->setDoctor($this);
-        }
-
-        return $this;
-    }
-
-    public function removePrescription(Prescription $prescription): static
-    {
-        if ($this->prescriptions->removeElement($prescription)) {
-            // set the owning side to null (unless already changed)
-            if ($prescription->getDoctor() === $this) {
-                $prescription->setDoctor(null);
-            }
-        }
 
         return $this;
     }
