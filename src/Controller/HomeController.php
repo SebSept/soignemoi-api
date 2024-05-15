c0a2c30605054ba40176157297e119887c557166
Sébastien Monterisi <sebastienmonterisi@yahoo.fr>
2024-05-15 16:52:15 +0200

    lancement des migrations lors de la mise à jour. a intégrer dans composer surement.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,9 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * SoigneMoi API - Projet ECF
+ *
+ * @author Sébastien Monterisi <sebastienmonterisi@gmail.com>
+ * 2024
+ */
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
