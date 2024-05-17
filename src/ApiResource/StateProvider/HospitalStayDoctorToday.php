6ba2dbdd81a2dee7bbe5042df4282f4a12fca2cd
Sébastien Monterisi <sebastienmonterisi@yahoo.fr>
2024-05-17 18:07:13 +0200

    utilisation d'un stateProvider à la place d'un controller
    https://api-platform.com/docs/core/controllers/

---
+++ src/ApiResource/StateProvider/HospitalStayDoctorToday.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * SoigneMoi API - Projet ECF
+ *
+ * @author Sébastien Monterisi <sebastienmonterisi@gmail.com>
+ * 2024
+ */
+
+namespace App\ApiResource\StateProvider;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProviderInterface;
+use App\Entity\HospitalStay;
+use App\Repository\HospitalStayRepository;
+use RuntimeException;
+
+/**
+ * Class HospitalStayDoctorToday.
+ *
+ * @implements ProviderInterface<HospitalStay>
+ */
+class HospitalStayDoctorToday implements ProviderInterface
+{
+    public function __construct(private readonly HospitalStayRepository $hospitalStayRepository)
+    {
+    }
+
+    /**
+     * @return HospitalStay[]
+     */
+    public function provide(Operation $operation, array $uriVariables = [], array $context = []): array
+    {
+        if (!isset($uriVariables['doctor_id'])) {
+            throw new RuntimeException('la variable doctor_id devrait être définie.');
+        }
+
+        assert(is_int($uriVariables['doctor_id']));
+
+        return $this->hospitalStayRepository->findByDoctorForToday($uriVariables['doctor_id']);
+    }
+}
