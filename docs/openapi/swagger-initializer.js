51f6640cda8fd6145eda586f7863c0e19f6bc4b7
Sébastien Monterisi <sebastienmonterisi@yahoo.fr>
2024-07-10 10:44:44 +0200

    documentation swagger : pas de possibilité de lancer des appels api
    car ne fonctionnerai pas.

--- docs/openapi/swagger-initializer.js
+++ docs/openapi/swagger-initializer.js
@@ -1,19 +1,33 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
+  const DisableTryItOutPlugin = function() {
+    return {
+      statePlugins: {
+        spec: {
+          wrapSelectors: {
+            allowTryItOutFor: () => () => false
+          }
+        }
+      }
+    }
+  }
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     url: "./docs.jsonopenapi",
     dom_id: '#swagger-ui',
+    tryItOutEnabled: false,
+    filter: false,
     deepLinking: true,
     presets: [
       SwaggerUIBundle.presets.apis,
       SwaggerUIStandalonePreset
     ],
     plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
+      SwaggerUIBundle.plugins.DownloadUrl,
+      DisableTryItOutPlugin
     ],
-    layout: "StandaloneLayout"
+    layout: "StandaloneLayout",
   });
 
   //</editor-fold>
